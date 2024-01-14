12f697bf5fb43ea3ee3e2b299a15f7aa942b9546
Igor Volostnov <visfin@yandex.ru>
2024-01-15 02:28:50 +0300

    Change 202401150228

--- extensions.py
+++ extensions.py
@@ -1,5 +1,46 @@
+import telebot
 import requests
 import json
+
+
+class BotTelegramCurrency(telebot.TeleBot):
+    def __init__(self, token, **kw):
+        telebot.TeleBot.__init__(self, token, **kw)
+        self.token = token
+        self.data = Currency()
+
+        @self.message_handler(commands=['start', 'help'])
+        def send_welcome(message):
+            whitespace = '\n'
+            self.reply_to(message, f"Привет, {message.from_user.first_name}, меня зовут Анжела! "
+                                   f"Я умею считать курсы валют! Для этого напиши мне сообщение в виде:{whitespace}"
+                                   f"<имя валюты, цену которой ты хочет узнать><пробел>{whitespace}"
+                                   f"<имя валюты, в которой надо узнать цену первой валюты><пробел>{whitespace}"
+                                   f"<количество первой валюты>.{whitespace}"
+                                   f"Чтобы посмотреть доступные валюты - напиши:{whitespace}"
+                                   f"/values{whitespace}")
+
+        @self.message_handler(commands=['values'])
+        def show_currency(message):
+            whitespace = '\n'
+            list_currency = []
+            for dict_ in self.data.arr_currency:
+                for key in dict_.keys():
+                    list_currency.append(dict_[key])
+            self.reply_to(message, f"Я умею считать следующие валюты:{whitespace}"
+                                   f"{whitespace.join(list_currency)}")
+
+        @self.message_handler(content_types=['text', ])
+        def send_price(message: telebot.types.Message):
+            list_info = message.text.strip().split()
+            self.data.set_base = list_info[0]
+            self.data.set_quote = list_info[1]
+            self.data.set_amount = list_info[2]
+            print(self.data.base, self.data.quote, self.data.amount)
+            self.reply_to(message, f"{self.data.get_price}")
+
+    def run(self):
+        self.polling(none_stop=True)
 
 
 class Currency:
@@ -8,7 +49,7 @@ class Currency:
         self.base = None
         self.quote = None
         self.amount = 1
-        self.arr_currency = []
+        self.arr_currency = [{'RUB': 'Рубль'}, {'USD': 'Доллар'}, {'EUR': 'Евро'}, {'BTC': 'Биткоин'}]
 
     @property
     def get_price(self):
@@ -28,9 +69,11 @@ class Currency:
     @set_base.setter
     def set_base(self, base):
         try:
-            if base in self.arr_currency:
-                self.base = base
-            else:
+            for d in self.arr_currency:
+                for currency_id, currency_str in d.items():
+                    if base == currency_str:
+                        self.base = currency_id
+            if self.base is None:
                 raise APIException('Я не знаю такой Валюты...Расскажете мне о ней?')
         except APIException as e:
             print(e)
@@ -42,9 +85,11 @@ class Currency:
     @set_quote.setter
     def set_quote(self, quote):
         try:
-            if quote in self.arr_currency:
-                self.quote = quote
-            else:
+            for d in self.arr_currency:
+                for currency_id, currency_str in d.items():
+                    if quote == currency_str:
+                        self.quote = currency_id
+            if self.quote is None:
                 raise APIException('Я не знаю такой Валюты...Расскажете мне о ней?')
         except APIException as e:
             print(e)
@@ -61,14 +106,6 @@ class Currency:
             print('Я наверное плохо объяснила, как у нас тут всё работает. Сначала валюта, которую переводим, '
                   'потом в какую переводим, а затем сколько  - в виде ЦЕЛОГО ЧИСЛА)))')
 
-    @property
-    def add_currency(self):
-        return self.arr_currency
-
-    @add_currency.setter
-    def add_currency(self, currency):
-        self.arr_currency.append(currency)
-
 
 class APIException(Exception):
     def __init__(self, *args):
@@ -82,14 +119,3 @@ class APIException(Exception):
             return 'Ниче не поняла..., {0} '.format(self.message)
         else:
             return 'Что-то я летаю в облаках, повторите, пожалуйста!'
-
-
-r = Currency()
-r.add_currency = 'USD'
-r.add_currency = 'EUR'
-r.add_currency = 'RUB'
-r.add_currency = 'BTC'
-r.set_base = 'BTC'
-r.set_quote = 'RUB'
-r.set_amount = 1
-print(r.get_price)
--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-import telebot
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from extensions import BotTelegramCurrency
 
 if __name__ == '__main__':
     load_dotenv()
-
-    bot = telebot.TeleBot(os.getenv('TOKEN'))
-
-
-    @bot.message_handler(commands=['start'])
-    def send_welcome(message):
-        bot.reply_to(message, f"Привет, {message.from_user.first_name}!")
-
-
-    @bot.message_handler(content_types=['photo', ])
-    def send_photo(message: telebot.types.Message):
-        bot.reply_to(message, f"Прикольная фотка XDD!")
-
-
-    bot.polling(none_stop=True)
+    bot = BotTelegramCurrency(os.getenv('TOKEN'))
+    bot.run()
